6259b7a252679faa251c4aabdff36f14fdb31f3c
= <=>
2024-06-03 11:15:03 +0800

    init commit

--- server/routes/accountRoutes.js
+++ server/routes/accountRoutes.js
@@ -2,7 +2,10 @@ import express from 'express'
 import { createFormResponse, getAccounts} from '../controllers/formresponseController.js'
 const router = express.Router();
 
-router.get('/', getAccounts);
-router.post('/', createFormResponse);
+router.get("/", function(req, res) {
+    res.status(200).send("Working");
+})
+router.get('/accounts', getAccounts);
+router.post('/accounts', createFormResponse);
 
 export default router
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import database from "./configs/dbconfig.js";
 import cors from "cors";
-import accountsRoute from "./routes/accountRoutes.js";
+import router from "./routes/accountRoutes.js";
 
 const PORT = 5000;
 const app = express();
@@ -15,7 +15,7 @@ try {
 
 app.use(cors());
 app.use(express.json());
-app.use("/accounts", accountsRoute);
+app.use("/", router);
 
 app.listen(PORT, () => {
   syncDatabase();
